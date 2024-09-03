5f1f95e0c32190ab02489929d2090b676e74aa49
bror <reachingbror@gmail.com>
2024-09-03 16:31:12 +0200

    Gjort en till funktion där uträkningen görs separat samt förtydligat koden och ändrat lite namn på funktioner och variabler. Förhoppningsvis är det mer överskådligt nu! Detta efter input från Erik

--- tip_calculator.py
+++ tip_calculator.py
@@ -34,32 +34,45 @@ def check_input():
 
 
 
-ci = check_input()
+ci = check_input() #Här tar vi inputen och kollar så den anges rätt.
 
-bill_amount = ci[0]
-tip_rate = ci[1]
-
-#tip_amount = bill_amount * (tip_rate / 100)
-#total = bill_amount + tip_amount
-#print(total)
-#print(tip_amount)
+bill_amount = ci[0] #Sparar notan
+tip_rate = ci[1] #Sparar procentvärdet av tip som vi vill betala
 
 
-def display_output(b,t):# Här visas tip, t och totalen b
-    tip_amount = b * (t / 100)
-    total = b + tip_amount
+def calculate_given_input(bill_input, tip_input):
 
-    print(f"The bill with non tip is {b:.2f}!") #detta printar b med 3 decimaler(:.3f) etc :.2f är 2 decimaler osv...!
-    print(f"The percentage tip is set to {t}%")
+    tip_amount = bill_input * (tip_input / 100)
+    total = bill_input + tip_amount
+
+    return total, tip_amount #Ger oss totalen att betala samt hur mycket som är tips
+
+
+
+calculations = calculate_given_input(bill_amount, tip_rate)
+
+
+tip_given = calculations[1]
+final_amount = calculations[0]
+
+
+
+
+
+def display_output(bill_without_tip, tip_percentage_given, output_tip, output_final_amount): # Här skickar vi med allt som ska visas. Notan utan tips, hur mycket tips vi vill ge, tips vi betalar, och slutsumman för notan med tips.
+
+
+
+    print(f"The bill with non tip is {bill_without_tip:.2f}!")
+    print(f"The percentage tip is set to {tip_percentage_given}%")
     print()
-    print(f'Total bill is {total:.2f}')
-    print(f'The tip you pay is {tip_amount:.2f}')
-
-
-    return t, b
+    print(f'Total bill is {output_final_amount:.2f}')
+    print(f'The tip you pay is {output_tip:.2f}')
 
 
 
 
 
-display = display_output(bill_amount, tip_rate)
+display = display_output(bill_amount, tip_rate, tip_given, final_amount)
+
+
