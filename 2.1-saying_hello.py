9ca233db2639bfe5e6ca6bc387d99fcc5b05423f
bror <reachingbror@gmail.com>
2024-09-05 11:39:52 +0200

    skapat en branch och filen

---
+++ 2.1-saying_hello.py
@@ -0,0 +1,20 @@
+#The “Hello, World” program is the first program you learn to write in many languages, but it doesn’t involve any input.
+#So create a program that prompts for your name and printsa greeting using your name.
+
+#Example Output
+#What is your name? Brian
+#Hello, Brian, nice to meet you!
+
+#Constraints
+#Different people deserve different greetings.
+
+#The default greeting is, as presented above, "Hello, [name], nice to meet you!"
+#But there are certain exceptions:
+
+#Here is a list names that should have certain greetings
+#"Glenn" -> "Hallåå eller änna Glenn"
+#"Alfred", "Erik" -> "Tjena brorsan"
+
+#And as in the previous excercise, try to structure your program into meaningful functions and be mindful in how you name them.
+
+def saying_hello():
