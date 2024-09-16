e45c5f6c8647dcd335ff83c6efd50d767b7084f0
bror <reachingbror@gmail.com>
2024-09-16 18:36:17 +0200

    Ett draft men kanske är jag redaan klar med denna uppgift? Gjort ett draftPR

--- 3-printing_quotes.py
+++ 3-printing_quotes.py
@@ -1,17 +1,9 @@
 
-
-
 def print_quotes():
     get_quotes = input('Enter any inspirational quotes about life, please!\n')
     get_author = input('Tell me the author or the mastermind behind that, i need to know!\n')
 
     return get_quotes, get_author
 
-
-
-
-
-
 start = print_quotes()
-
-print(start[0] +' ' + start[1])
+print('A wise dude told me something great \"' + start[0] + '\" ' +'And that dude is the almighty ' + start[1].capitalize() +'.')
