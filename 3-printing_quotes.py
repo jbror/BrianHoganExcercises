d24c9e2438a9c3166973de6b3004bb736febcfe4
bror <reachingbror@gmail.com>
2024-09-16 15:25:10 +0200

    jobbar med uppgiften, ej klar. ATM - Draft + BUP

--- 3-printing_quotes.py
+++ 3-printing_quotes.py
@@ -1,1 +1,17 @@
-# Skriv din kod här :-)
+
+
+
+def print_quotes():
+    get_quotes = input('Enter any inspirational quotes about life, please!\n')
+    get_author = input('Tell me the author or the mastermind behind that, i need to know!\n')
+
+    return get_quotes, get_author
+
+
+
+
+
+
+start = print_quotes()
+
+print(start[0] +' ' + start[1])
