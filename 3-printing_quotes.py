08d7aaf69991d805d30012ae5426a3345d6d200f
bror <reachingbror@gmail.com>
2024-09-17 15:51:01 +0200

    Ändrat namnet på funktionen

--- 3-printing_quotes.py
+++ 3-printing_quotes.py
@@ -1,9 +1,9 @@
 
-def print_quotes():
+def get_quotes():
     get_quotes = input('Enter any inspirational quotes about life, please!\n')
     get_author = input('Tell me the author or the mastermind behind that, i need to know!\n')
 
     return get_quotes, get_author
 
-start = print_quotes()
+start = get_quotes()
 print('A wise dude told me something great \"' + start[0] + '\" ' +'And that dude is the almighty ' + start[1].capitalize() +'.')
