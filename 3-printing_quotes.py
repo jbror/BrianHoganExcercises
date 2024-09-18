91c338f35e0ebc29cf6da969a1e1ca359bbacbde
bror <reachingbror@gmail.com>
2024-09-18 16:00:44 +0200

    Gör andra uppgiften aka challange

--- 3-printing_quotes.py
+++ 3-printing_quotes.py
@@ -1,4 +1,4 @@
-
+"""
 def get_quotes():
     get_quotes = input('Enter any inspirational quotes about life, please!\n')
     get_author = input('Tell me the author or the mastermind behind that, i need to know!\n')
@@ -7,3 +7,23 @@ def get_quotes():
 
 start = get_quotes()
 print('A wise dude told me something great \"' + start[0] + '\" ' +'And that dude is the almighty ' + start[1].capitalize() +'.')
+"""
+
+
+
+#Uppgiften under "Challange" från boken.
+
+
+def structure_holding_quotes():
+    quotes_authors = [
+    'hej',
+    'kaka',
+    {'box': 44}
+    ]
+
+    print(type(quotes_authors[2]['box']))
+    print(quotes_authors)
+
+
+
+structure_holding_quotes()
