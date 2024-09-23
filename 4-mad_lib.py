7e07393f7c330db0801748c8effd86eeb5be9df4
Bror Johansson <bror@Brors-MBPP.lan>
2024-09-23 17:37:49 +0200

    Pular med uppgiften och jobbar vidare. Bara draft atm

--- 4-mad_lib.py
+++ 4-mad_lib.py
@@ -1,1 +1,11 @@
-# Skriv din kod här :-)
+def mad_lib():
+
+    prompt_noun = input('Hey! Give me a Noun:  ')
+    prompt_verb = input('I need a Verb from you, enter Verb: ')
+    prompt_adjective = input('Also give me an Adjective: ')
+    prompt_adverb = input('Enter an Adverb or die : ')
+
+
+
+
+start = mad_lib()
