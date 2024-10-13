c92a0a6e4e36b1032cd1c0c3a82353bd913b4eac
bror <reachingbror@gmail.com>
2024-10-13 13:44:11 +0200

    jobbar vidare med uppgiften

--- 5-simple_math.py
+++ 5-simple_math.py
@@ -5,13 +5,18 @@ def prompt_input():
     first_num = input('Hi! Give me number ')
     second_num = input('I need a number again, pls! ')
 
-
+    return first_num, second_num
 
 def sums(n1, n2):
-
+    return n1 + n2
 
 
 
 
 start = prompt_input()
 
+n1 = float(start[0])
+n2 = float(start[1])
+
+
+plus = sums(n1, n2)
