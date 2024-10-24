c990019c63d4dba4b2810247a9c395a35a02b6ba
bror <reachingbror@gmail.com>
2024-10-24 15:29:34 +0200

    Fortsätter med uppgiften, ej klar. Ska printa ut allt näst från mina funktioner

--- 6-retirement_calculator.py
+++ 6-retirement_calculator.py
@@ -1,19 +1,23 @@
 #Retirement Calculator
 import datetime
+import time
 
 def prompt_ages():
-    try:
-        prompt_age = float(input('What\'s your age?'))
-        prompt_retirement_age = float(input('At what age would you like to retire in?'))
-        return prompt_age, prompt_retirement_age
-    except ValueError:
-        print('Only floats!')
+    while True:
+        try:
+            prompt_age = float(input('What\'s your age? '))
+            prompt_retirement_age = float(input('At what age would you like to retire in? '))
+            return prompt_age, prompt_retirement_age
+        except ValueError:
+            print('Only floats!')
 
 
 def count_years(current_age, age_retirement):
     return age_retirement - current_age
-    #print(age_retirement - current_age)
 
+
+
+#display_years_until_freedom =
 
 
 
@@ -23,25 +27,12 @@ start = prompt_ages()
 age = start[0]
 retirement_age = start[1]
 
-
-
 check_years = count_years(age, retirement_age)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(f'Hello! I will soon inform you how long it is left until total freedom.\nPLEASE be seated... \n')
+time.sleep(3)
 
 
 
