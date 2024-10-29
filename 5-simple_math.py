bc95c9fc4996a15977f636c797634ec491000dd6
Bror Johansson <reachingbror@gmail.com>
2024-10-29 16:53:52 +0100

    5-simple_math (#7)
    * skapar filen för att börja uppgiften
    
    * börjat arbeta med uppgiften och skapat en funktion
    
    * fortsätter med uppgiften bara
    
    * jobbar vidare med uppgiften
    
    * Gjort funktioner för varje uträkning och börjat med att printa ut allt snyggt. Ej klar men näramar mig
    
    * Gjort outputen i en lång outputsträng och är kanske klar med uppgiften
    
    * ändrade namn på funktioner och variabler till mer passaande namn
    
    * tog bort floats som redan var floats

---
+++ 5-simple_math.py
@@ -0,0 +1,32 @@
+def prompt_input():
+    while True:
+        try:
+            first_num = float(input('Hi! Give me number '))
+            second_num = float(input('I need a number again, pls! '))
+            return first_num, second_num
+        except ValueError:
+            print('Only numbers!')
+def plus(n1, n2):
+    return n1 + n2
+
+def minus(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divided(n1, n2):
+    return n1 / n2
+
+start = prompt_input()
+
+n1 = (start[0])
+n2 = (start[1])
+
+sums = plus(n1, n2)
+difference = minus(n1, n2)
+product = multiply(n1, n2)
+quotient = divided(n1, n2)
+
+print(f'What is the first number? {n1} \nWhat is the second number? {n2}\n{n1} + {n2} = {sums}\n\
+{n1} - {n2} = {difference}\n{n1} * {n2} = {product}\n{n1} / {n2} = {quotient}')
