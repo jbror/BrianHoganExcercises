9429d69366e07fbff7d7498f1ebc6b092fb1c04b
bror <reachingbror@gmail.com>
2024-11-04 15:00:57 +0100

    Tror jag gjort klart uppgiften och ska nu kolla med Erik om han tycker den är godkänd

--- 6-retirement_calculator.py
+++ 6-retirement_calculator.py
@@ -13,37 +13,21 @@ def prompt_ages():
 
 
 def count_years(current_age, age_retirement):
-    return age_retirement - current_age
+    current_time = datetime.datetime.now()
 
-
-
-#display_years_until_freedom =
-
-
+    return age_retirement - current_age + current_time.year, age_retirement - current_age
 
 
 start = prompt_ages()
-
 age = start[0]
 retirement_age = start[1]
-
 check_years = count_years(age, retirement_age)
-
-
+current_time = datetime.datetime.now()
 
 print(f'Hello! I will soon inform you how long it is left until total freedom.\nPLEASE be seated... \n')
 time.sleep(3)
 
+print(f'You are now {age:.0f} years old. You want to retire when you are {retirement_age:.0f} years old. \
+The year is now {current_time.year} and the year when total freedom starts(aka retirement) for you is {check_years[0]:.0f}. \
+That is only {check_years[1]:.0f} years away!! Prepare and enjoy =)')
 
-
-print(f'You are now {age:.0f} years old. You want to retire when you are {retirement_age} years old..')
-
-
-
-"""
-current_time = datetime.datetime.now()
-
-print(current_time.year)
-print(current_time)
-
-"""
