bbbda9e83910793bee45c2a3f0dc9f9f9b09b70a
bror <reachingbror@gmail.com>
2024-11-05 15:05:48 +0100

    Ändrat namn på funktioner och variabler samt gjort strukturen i koden bättre.

--- 6-retirement_calculator.py
+++ 6-retirement_calculator.py
@@ -12,22 +12,22 @@ def prompt_ages():
             print('Only floats!')
 
 
-def count_years(current_age, age_retirement):
+def calculate_retirement_year(current_age, chosen_year_for_retirement):
     current_time = datetime.datetime.now()
+    calculate_retirement_year = chosen_year_for_retirement - current_age + current_time.year
+    calculate_years_to_freedom = chosen_year_for_retirement - current_age
+    return calculate_retirement_year, calculate_years_to_freedom
 
-    return age_retirement - current_age + current_time.year, age_retirement - current_age
 
-
-start = prompt_ages()
-age = start[0]
-retirement_age = start[1]
-check_years = count_years(age, retirement_age)
+start_prompt = prompt_ages()
+current_age = start_prompt[0]
+chosen_year_for_retirement = start_prompt[1]
+years_to_retirement = calculate_retirement_year(current_age, chosen_year_for_retirement)#Denna retunerar två värden/strängar. Året du pensionerar dig (total_freedom_year) och hur många år det är kvar till det året.
+total_freedom_year = years_to_retirement[0]
 current_time = datetime.datetime.now()
 
 print(f'Hello! I will soon inform you how long it is left until total freedom.\nPLEASE be seated... \n')
 time.sleep(3)
-
-print(f'You are now {age:.0f} years old. You want to retire when you are {retirement_age:.0f} years old. \
-The year is now {current_time.year} and the year when total freedom starts(aka retirement) for you is {check_years[0]:.0f}. \
-That is only {check_years[1]:.0f} years away!! Prepare and enjoy =)')
-
+print(f'You are now {current_age:.0f} years old. You want to retire when you are {chosen_year_for_retirement:.0f} years old. \
+The year is now {current_time.year} and the year when total freedom starts(aka retirement) for you is {total_freedom_year:.0f}. \
+That is only {years_to_retirement[1]:.0f} years away!! Prepare and enjoy =)')
