78669dca6e8fdb23b196e40f56c871a6eb572539
bror <reachingbror@gmail.com>
2024-11-12 16:51:38 +0100

    börjar med funktion som ber om input, work in progress

--- 7-area_of_a_rectangular_room.py
+++ 7-area_of_a_rectangular_room.py
@@ -1,1 +1,16 @@
-print('tjo')
+print('yo')
+#draft atm...!
+
+def prompt_length_width_in_feet(prompt):
+    while True:
+        try:
+            area_data = float(input(prompt))
+            return area_data
+        except ValueError:
+            print('Just floats')
+
+
+
+
+length_feet = prompt_length_width_in_feet('Howdy! What is the length of the room in feet? (I know that it\'s a sucky way to measure btw..!)\n')
+width_feet = prompt_length_width_in_feet('And what is the width of the room in feet?\n')
