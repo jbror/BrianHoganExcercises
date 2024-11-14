ba9bb25a1977e54b81d28f1d89a21eb782ec38ec
bror <reachingbror@gmail.com>
2024-11-14 16:48:38 +0100

    fortsatt jobba på uppgiften och skapat en till funktion som beräknar arean. Ändrat namn på funktioner och variabler efter Erik sågat mig. Bara draft och BUP still.

--- 7-area_of_a_rectangular_room.py
+++ 7-area_of_a_rectangular_room.py
@@ -1,7 +1,7 @@
 print('yo')
 #draft atm...!
 
-def prompt_length_width_in_feet(prompt):
+def prompt_length_width(prompt):
     while True:
         try:
             area_data = float(input(prompt))
@@ -10,7 +10,13 @@ def prompt_length_width_in_feet(prompt):
             print('Just floats')
 
 
+def calculate_area(length, width):
+    square_unit = length * width
+    print(square_unit)
 
 
-length_feet = prompt_length_width_in_feet('Howdy! What is the length of the room in feet? (I know that it\'s a sucky way to measure btw..!)\n')
-width_feet = prompt_length_width_in_feet('And what is the width of the room in feet?\n')
+length_feet = prompt_length_width('Howdy! What is the length of the room in feet? (I know that it\'s a sucky way to measure btw..!)\n')
+width_feet = prompt_length_width('And what is the width of the room in feet?\n')
+
+
+calculate_area(length_feet, width_feet)
