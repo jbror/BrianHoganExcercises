b641d08718bd74146f2d447087207fb17c51c9cc
bror <reachingbror@gmail.com>
2025-01-15 21:21:44 +0100

    working the conversion output, draft bup atm

--- 7-area_of_a_rectangular_room.py
+++ 7-area_of_a_rectangular_room.py
@@ -12,9 +12,15 @@ def prompt_length_width(prompt):
 
 def calculate_area(length, width):
     square_unit = length * width
-    #square_feet =
-    #square_meters =
-    print(square_unit)
+
+    square_feet = round(square_unit)
+    square_meters = square_unit * 0.09290304
+
+    rounded_square_meters = round(square_meters, 3)
+
+
+    print(square_feet)
+    print(rounded_square_meters)
 
 
 length_feet = prompt_length_width('Howdy! What is the length of the room in feet? (I know that it\'s a sucky way to measure btw..!)\n')
