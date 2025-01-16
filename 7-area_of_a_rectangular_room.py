a6def85ac857f14ee64cd85a4c8a07e2e1efcca4
bror <reachingbror@gmail.com>
2025-01-16 12:34:30 +0100

    I think i got it done. Got two functions one that calculate and one that prompt for input. Now i pray that this does not suck

--- 7-area_of_a_rectangular_room.py
+++ 7-area_of_a_rectangular_room.py
@@ -1,6 +1,3 @@
-print('yo')
-#draft atm...!
-
 def prompt_length_width(prompt):
     while True:
         try:
@@ -12,19 +9,17 @@ def prompt_length_width(prompt):
 
 def calculate_area(length, width):
     square_unit = length * width
-
     square_feet = round(square_unit)
-    square_meters = square_unit * 0.09290304
+    square_meters = round(square_unit * 0.09290304, 3)
+    return square_feet, square_meters
 
-    rounded_square_meters = round(square_meters, 3)
 
-
-    print(square_feet)
-    print(rounded_square_meters)
 
 
 length_feet = prompt_length_width('Howdy! What is the length of the room in feet? (I know that it\'s a sucky way to measure btw..!)\n')
 width_feet = prompt_length_width('And what is the width of the room in feet?\n')
+calc_area = calculate_area(length_feet, width_feet) # Should/could i name the var the same-ish as the func :P?
+display_output_feet, display_output_meters = calc_area
 
-
-calculate_area(length_feet, width_feet)
+print(display_output_feet)
+print(display_output_meters)
