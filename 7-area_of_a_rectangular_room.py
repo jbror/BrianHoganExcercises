56d1cf4995a75409f8b00db05cfa54ff4ec8b26a
bror <reachingbror@gmail.com>
2025-01-21 18:57:11 +0100

    Changed the function and some names after PR-reply

--- 7-area_of_a_rectangular_room.py
+++ 7-area_of_a_rectangular_room.py
@@ -1,4 +1,4 @@
-def prompt_length_width(prompt):
+def prompt_float(prompt):
     while True:
         try:
             area_data = float(input(prompt))
@@ -8,18 +8,17 @@ def prompt_length_width(prompt):
 
 
 def calculate_area(length, width):
-    square_unit = length * width
-    square_feet = round(square_unit)
-    square_meters = round(square_unit * 0.09290304, 3)
-    return square_feet, square_meters
+    area = length * width
+    return area
 
 
 
+area_length = prompt_float('Howdy! What is the length of the room in feet? (I know that it\'s a sucky way to measure btw..!)\n')
+area_width = prompt_float('And what is the width of the room in feet?\n')
 
-length_feet = prompt_length_width('Howdy! What is the length of the room in feet? (I know that it\'s a sucky way to measure btw..!)\n')
-width_feet = prompt_length_width('And what is the width of the room in feet?\n')
-calc_area = calculate_area(length_feet, width_feet) # Should/could i name the var the same-ish as the func :P?
-display_output_feet, display_output_meters = calc_area
+square_feet = calculate_area(area_length, area_width)
+square_meters = square_feet * 0.09290304
 
-print(display_output_feet)
-print(display_output_meters)
+print('This is the square feet and square meters(rounded with 3 decimals!)')
+print(square_feet, str('Square feet'))
+print(round(square_meters,3),str('Square meters'))
