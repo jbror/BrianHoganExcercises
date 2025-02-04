74fd1ece7f1f9e7790a325bbc0fd7a2eab9e0bdc
bror <reachingbror@gmail.com>
2025-02-04 03:30:09 +0100

    Finished with the assignment. Got 3 functions that handles input, calaculations and giving output. I like this way of doing it!

--- 8-pizza_party.py
+++ 8-pizza_party.py
@@ -1,5 +1,4 @@
 #Pizza Party!
-
 def prompt_int(prompt_message):
     while True:
         try:
@@ -8,25 +7,21 @@ def prompt_int(prompt_message):
         except ValueError:
             print('Just numbers..')
 
-
-
-def pizza_calculator(people,pizzas,slices):
+def pizza_calculator(people, pizzas, slices=8):
     total_slices = pizzas * slices
     slices_per_person =  total_slices // people
     leftovers = total_slices % people
-    #just testing below!
-    print(f'You are {number_of_humans} humans in this room')
-    print('Total slices:',total_slices)
-    print('And all humans will get:', slices_per_person)
-    print('Leftover slices:', leftovers)
+    return people, pizzas, slices, total_slices, slices_per_person, leftovers
 
+def display_pizza_information(people, pizzas, slices, total_slices, slices_per_person, leftovers):
+    print(f'Alright.. {people} people with {pizzas} pizzas sliced in {slices} slices per pizza')
+    print(f'Total slices: {total_slices}')
+    print(f'And all humans will get {slices_per_person} slices')
+    print(f'Slice leftover count: {leftovers}')
 
+number_of_humans = prompt_int('How many humans will eat?\n')
+number_of_pizzas = prompt_int('And how many pizzas will you have and slay in your mouth?\n')
+number_of_slices = prompt_int('Tell me how many slices you like per pizza. 8 is standard on planet earth\n')
 
-
-
-
-number_of_humans = prompt_int('How many humans will eat?')
-number_of_pizzas = prompt_int('And how many pizzas will you have and slay in your mouth?')
-number_of_slices = prompt_int('Tell me how many slices you like per pizza!')
-
-info = pizza_calculator(number_of_humans,number_of_pizzas,number_of_slices)
+pizza_data = pizza_calculator(number_of_humans,number_of_pizzas,number_of_slices)
+display_pizza_information(*pizza_data)
