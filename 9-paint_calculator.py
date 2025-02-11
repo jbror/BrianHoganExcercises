7e4adfc8bb7d726d2b5de4e437d762c5f7bbbb53
bror <reachingbror@gmail.com>
2025-02-11 13:49:40 +0100

    just taking some code i used earlier

--- 9-paint_calculator.py
+++ 9-paint_calculator.py
@@ -1,1 +1,18 @@
-#9!
+#Paint Calculator
+
+
+
+
+def prompt_int(prompt_message):
+    while True:
+        try:
+            prompt = int(input(prompt_message))
+            return prompt
+        except ValueError:
+            print('Just numbers..')
+
+
+
+
+ceiling_length = prompt_int('Tell me the length of the ceiling\n')
+ceiling_width = prompt_int('Tell me the width of the ceiling\n')
