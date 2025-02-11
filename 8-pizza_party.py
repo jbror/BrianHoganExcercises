1b080e93964d523b2161a5a0d0509a3d732b6195
Bror Johansson <reachingbror@gmail.com>
2025-02-11 13:29:28 +0100

    Exercise 8 - Pizza Party (#13)
    * Creating pyscript
    
    * started with the function that takes input and also the function that will handle the calculations. Still work in progress but got a neat start.
    
    * Finished with the assignment. Got 3 functions that handles input, calaculations and giving output. I like this way of doing it!
    
    * Got some feedback in PR and i made some changes now

---
+++ 8-pizza_party.py
@@ -0,0 +1,29 @@
+#Pizza Party!
+def prompt_int(prompt_message):
+    while True:
+        try:
+            prompt = int(input(prompt_message))
+            return prompt
+        except ValueError:
+            print('Just numbers..')
+
+def pizza_calculator(people, pizzas, slices=8):
+    total_slices = pizzas * slices
+    slices_per_person =  total_slices // people
+    leftovers = total_slices % people
+    return total_slices, slices_per_person, leftovers
+
+def display_pizza_information(people, pizzas, slices, total_slices, slices_per_person, leftovers):
+    print(f'Alright.. {people} people with {pizzas} pizzas sliced in {slices} slices per pizza')
+    print(f'Total slices: {total_slices}')
+    print(f'And all humans will get {slices_per_person} slices')
+    print(f'Slice leftover count: {leftovers}')
+
+number_of_humans = prompt_int('How many humans will eat?\n')
+number_of_pizzas = prompt_int('And how many pizzas will you have and slay in your mouth?\n')
+number_of_slices_per_pizza = prompt_int('Tell me how many slices you like per pizza. 8 is standard on planet earth\n')
+
+total_slices, slices_per_person, leftovers = pizza_calculator(number_of_humans,number_of_pizzas,number_of_slices_per_pizza)
+
+display_pizza_information(number_of_humans, number_of_pizzas, number_of_slices_per_pizza, total_slices, slices_per_person,
+leftovers)
