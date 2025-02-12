fc88aeee25221e86d968ce208592da3fb7908b0a
bror <reachingbror@gmail.com>
2025-02-12 13:59:20 +0100

    just putting things together and working on how i should collect the prices and items. Work in progress

--- 10-self-checkout.py
+++ 10-self-checkout.py
@@ -1,7 +1,6 @@
 #10 Self-Checkout
-import math # to use the Python math.ceil() method
+import math #
 
-PAINT_COVERAGE = 350 # 1 gallon per 350 square feet
 
 def prompt_float(prompt):
     while True:
@@ -21,12 +20,20 @@ def calculate_gallons_needed(area): # How many whole gallons is needed? Find out
     return math.ceil(area / PAINT_COVERAGE) # Rounds up to next whole number!
 
 
-area_length = prompt_float('Howdy! What is the length of the room in feet? (I know that it\'s a sucky way to measure btw..!)\n')
-area_width = prompt_float('And what is the width of the room in feet?\n')
+price_of_item = prompt_float('Enter price of item 1:\n')
+quanity_of_item = prompt_float('Enter the quanity of item 1:\n')
+
+item_and_price = # store in this? Dictonary maybe?
+
+
+
+
+"""
+
 square_feet = calculate_area(area_length, area_width) # Get the area of the room in square feet
 gallons_needed_count = calculate_gallons_needed(square_feet) # Display the output
 
 print(f'You will need to buy {gallons_needed_count} gallons of paint to cover {square_feet} square feet.')
 
 
-# Write your code here :-)
+"""
